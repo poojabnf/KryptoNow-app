c2a3e241fc7af953c0c73e26bf1c002075288cb8
Sanjay Kumar <kumarsanjayyadav35@gmail.com>
2026-04-30 22:43:37 +0530

    fix: add Vercel web build config for Expo

---
+++ index.web.js
@@ -0,0 +1,11 @@
+﻿import "react-native-url-polyfill/auto";
+import "react-native-get-random-values";
+import { registerRootComponent } from "expo";
+import { ExpoRoot } from "expo-router";
+
+export function App() {
+  const ctx = require.context("./app");
+  return <ExpoRoot context={ctx} />;
+}
+
+registerRootComponent(App);
