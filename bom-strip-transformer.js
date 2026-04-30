ab027638875d35e4eb955f22f88c952ba9e4c0f6
Sanjay Kumar <kumarsanjayyadav35@gmail.com>
2026-04-30 18:33:57 +0530

    chore: remove debug scripts and junk files

--- bom-strip-transformer.js
+++
@@ -1,8 +0,0 @@
-const upstream = require('@expo/metro-config/build/babel-transformer');
-
-module.exports.transform = function (params) {
-  if (params.src && params.src.charCodeAt(0) === 0xFEFF) {
-    params = { ...params, src: params.src.slice(1) };
-  }
-  return upstream.transform(params);
-};
