82158aab757db48d7788c8390b8a6b31377a4397
Sanjay Kumar <kumarsanjayyadav35@gmail.com>
2026-05-02 08:43:01 +0530

    fix: hardcode Alchemy URLs in chains.ts + remove stale dist [2026-05-02 08:43:01]

--- utils/chains.ts
+++ utils/chains.ts
@@ -1,7 +1,5 @@
 ﻿import { ethers } from 'ethers'
 
-const ALCHEMY = process.env.EXPO_PUBLIC_ALCHEMY_KEY ?? 't7T7fcsMA4rqQYH70YRV3'
-
 export type Chain = {
   id:          number
   name:        string
@@ -15,12 +13,12 @@ export type Chain = {
 }
 
 export const CHAINS: Chain[] = [
-  { id: 1,     name: 'Ethereum',  symbol: 'ETH',   nativeName: 'Ether', icon: 'E',  color: '#627EEA', rpc: `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY}`,     explorer: 'https://etherscan.io',            coingeckoId: 'ethereum'      },
-  { id: 137,   name: 'Polygon',   symbol: 'MATIC', nativeName: 'Matic', icon: 'P',  color: '#8247E5', rpc: `https://polygon-mainnet.g.alchemy.com/v2/${ALCHEMY}`, explorer: 'https://polygonscan.com',         coingeckoId: 'matic-network' },
-  { id: 56,    name: 'BNB Chain', symbol: 'BNB',   nativeName: 'BNB',   icon: 'B',  color: '#F0B90B', rpc: 'https://bsc-dataseed1.binance.org/',                   explorer: 'https://bscscan.com',             coingeckoId: 'binancecoin'   },
-  { id: 42161, name: 'Arbitrum',  symbol: 'ETH',   nativeName: 'Ether', icon: 'A',  color: '#2D374B', rpc: `https://arb-mainnet.g.alchemy.com/v2/${ALCHEMY}`,     explorer: 'https://arbiscan.io',             coingeckoId: 'ethereum'      },
-  { id: 10,    name: 'Optimism',  symbol: 'ETH',   nativeName: 'Ether', icon: 'O',  color: '#FF0420', rpc: `https://opt-mainnet.g.alchemy.com/v2/${ALCHEMY}`,     explorer: 'https://optimistic.etherscan.io', coingeckoId: 'ethereum'      },
-  { id: 8453,  name: 'Base',      symbol: 'ETH',   nativeName: 'Ether', icon: 'Ba', color: '#0052FF', rpc: `https://base-mainnet.g.alchemy.com/v2/${ALCHEMY}`,    explorer: 'https://basescan.org',            coingeckoId: 'ethereum'      },
+  { id: 1,     name: 'Ethereum',  symbol: 'ETH',   nativeName: 'Ether', icon: 'E',  color: '#627EEA', rpc: 'https://eth-mainnet.g.alchemy.com/v2/t7T7fcsMA4rqQYH70YRV3',     explorer: 'https://etherscan.io',            coingeckoId: 'ethereum'      },
+  { id: 137,   name: 'Polygon',   symbol: 'MATIC', nativeName: 'Matic', icon: 'P',  color: '#8247E5', rpc: 'https://polygon-mainnet.g.alchemy.com/v2/t7T7fcsMA4rqQYH70YRV3', explorer: 'https://polygonscan.com',         coingeckoId: 'matic-network' },
+  { id: 56,    name: 'BNB Chain', symbol: 'BNB',   nativeName: 'BNB',   icon: 'B',  color: '#F0B90B', rpc: 'https://bsc-dataseed1.binance.org/',                               explorer: 'https://bscscan.com',             coingeckoId: 'binancecoin'   },
+  { id: 42161, name: 'Arbitrum',  symbol: 'ETH',   nativeName: 'Ether', icon: 'A',  color: '#2D374B', rpc: 'https://arb-mainnet.g.alchemy.com/v2/t7T7fcsMA4rqQYH70YRV3',     explorer: 'https://arbiscan.io',             coingeckoId: 'ethereum'      },
+  { id: 10,    name: 'Optimism',  symbol: 'ETH',   nativeName: 'Ether', icon: 'O',  color: '#FF0420', rpc: 'https://opt-mainnet.g.alchemy.com/v2/t7T7fcsMA4rqQYH70YRV3',     explorer: 'https://optimistic.etherscan.io', coingeckoId: 'ethereum'      },
+  { id: 8453,  name: 'Base',      symbol: 'ETH',   nativeName: 'Ether', icon: 'Ba', color: '#0052FF', rpc: 'https://base-mainnet.g.alchemy.com/v2/t7T7fcsMA4rqQYH70YRV3',    explorer: 'https://basescan.org',            coingeckoId: 'ethereum'      },
 ]
 
 export function getProvider(chain: Chain): ethers.JsonRpcProvider {
